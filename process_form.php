0826bdacb607ebaa6e0eef859e3953260b0e5435
sisay <sisaybekele735@gmail.com>
2024-02-15 01:13:12 +0300

    instagram clone added

---
+++ process_form.php
@@ -0,0 +1,25 @@
+<!-- <?php
+
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1)
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $name = $_POST["name"];
+//     $email = $_POST["email"];
+//     $message = $_POST["message"];
+
+//     // Add your email address where you want to receive messages
+//     $to = "sisbek2143@gmail.com";
+//     $subject = "New Contact Form Submission";
+//     $headers = "From: $email";
+
+//     mail($to, $subject, $message, $headers);
+
+//     // Redirect after successful submission
+//     header("Location: thank_u.html");
+//     exit();
+// } else {
+//     // Redirect if accessed directly without submitting the form
+//     header("Location: index.html");
+//     exit();
+// }
+?> -->
